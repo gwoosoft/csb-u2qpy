c11b4de9299b336d312f8074d0ace9b151af856a
Code Sandbox <hello@codesandbox.io>
2021-07-29 06:06:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.461af35e31b92d01dbae341673fa05cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb167c7804940b2c6556db94de32892",
+    "url": "https://gwoosoft.github.io/csb-u2qpy/index.html"
+  },
+  {
+    "revision": "cc7f9719594b0562eb1f",
+    "url": "https://gwoosoft.github.io/csb-u2qpy/static/js/2.c996534d.chunk.js"
+  },
+  {
+    "revision": "25143832e4c237419ca2",
+    "url": "https://gwoosoft.github.io/csb-u2qpy/static/js/main.ee46fad6.chunk.js"
+  },
+  {
+    "revision": "a929074933da602079d5",
+    "url": "https://gwoosoft.github.io/csb-u2qpy/static/js/runtime-main.d46c1c60.js"
+  }
+]);
